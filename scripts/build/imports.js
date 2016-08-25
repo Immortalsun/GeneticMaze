f7a457ef92ffe49ee9563880ffe448b42b147487
Immortalsun <maashes.p@gmail.com>
2016-08-24 22:13:48 -0400

    Add first files to set up project structure

---
+++ scripts/build/imports.js
@@ -0,0 +1,7 @@
+// This file is to be used for all necessary require
+// statements for the entire scripts folder.
+//Imports
+var fs = require('fs');
+const electron = require('electron');
+const remote = electron.remote;
+const dialog = remote.dialog; 
